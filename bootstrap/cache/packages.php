7f67e0e233a31b47e6255cba42c6a9f2e01499a7
NLZ <nfx.gilo@gmail.com>
2018-08-16 13:11:02 +0200

    Add files via upload

---
+++ bootstrap/cache/packages.php
@@ -0,0 +1,88 @@
+<?php return array (
+  'fideloper/proxy' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Fideloper\\Proxy\\TrustedProxyServiceProvider',
+    ),
+  ),
+  'laravel/tinker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Laravel\\Tinker\\TinkerServiceProvider',
+    ),
+  ),
+  'nunomaduro/collision' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'NunoMaduro\\Collision\\Adapters\\Laravel\\CollisionServiceProvider',
+    ),
+  ),
+  'intervention/image' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Intervention\\Image\\ImageServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Image' => 'Intervention\\Image\\Facades\\Image',
+    ),
+  ),
+  'graham-campbell/markdown' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'GrahamCampbell\\Markdown\\MarkdownServiceProvider',
+    ),
+  ),
+  'grafite/crypto' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Grafite\\Crypto\\CryptoProvider',
+    ),
+  ),
+  'laravelcollective/html' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Collective\\Html\\HtmlServiceProvider',
+    ),
+    'aliases' => 
+    array (
+      'Form' => 'Collective\\Html\\FormFacade',
+      'Html' => 'Collective\\Html\\HtmlFacade',
+    ),
+  ),
+  'grafite/formmaker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Grafite\\FormMaker\\FormMakerProvider',
+    ),
+  ),
+  'grafite/crudmaker' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Grafite\\CrudMaker\\CrudMakerProvider',
+    ),
+  ),
+  'grafite/builder' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Grafite\\Builder\\GrafiteBuilderProvider',
+    ),
+  ),
+  'grafite/cms' => 
+  array (
+    'providers' => 
+    array (
+      0 => 'Grafite\\Cms\\GrafiteCmsProvider',
+    ),
+  ),
+);
